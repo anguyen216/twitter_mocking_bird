5b2ba1fc55f6c740bd55c5ac5e5adc8da4ce9d76
Anh Nguyen <nguyena.r216@gmail.com>
2021-03-04 22:48:30 -0500

    config home screen; add logout func

---
+++ Twitter/homeTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  homeTableViewController.swift
+//  Twitter
+//
+//  Created by Anh Nguyen on 3/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class homeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
